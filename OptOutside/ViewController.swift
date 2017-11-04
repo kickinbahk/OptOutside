0351f4b56200a028f559ace3f7fd9379219032c7
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-11-04 11:33:22 -0700

    display first prompt when app starts

--- OptOutside/ViewController.swift
+++ OptOutside/ViewController.swift
@@ -21,7 +21,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        promptLabel.text = Prompts.whatToDo
     }
     
 
